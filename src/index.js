12115422775e7f8996c7b845eb3de59a15b94266
AmitKumarSharma4046 <aerez4046@gmail.com>
2022-04-26 17:04:55 +0530

    new start

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./route/route');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://amit-DB:amit3112@cluster0.eztoe.mongodb.net/amit-DB?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
